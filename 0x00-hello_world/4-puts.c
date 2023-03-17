5ccbe53cc0fb50d694a3533d8113836c2f1a5ad8
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-17 08:44:22 -0700

    File 4-puts.c up to date

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point, prints on the screen
  * Return: Always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
