2baba3b34bff89645ba156278a1e29e172cb5fb4
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-22 20:30:58 -0700

    File 7-print_last_digit.c fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,15 @@
 int print_last_digit(int x)
 {
 	x %= 10;
-
 	if (x < 0)
 
 {
-	x *= -1;
-	_putchar(x + '0');
+	_putchar(-x + '0');
+	return (-x);
 }
+	else
+{
+	_putchar(x + '0');
 	return (x);
+}
 }
