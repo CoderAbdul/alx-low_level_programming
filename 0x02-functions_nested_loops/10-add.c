40eaf815673644ccfa3467dda5c6ce2f8d4c56e9
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-22 18:44:18 -0700

    File 10-add.c added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers numbers
+ * @j: parameter 1
+ * @i: parameter 2
+ *
+ * Return: returns the result
+ */
+int add(int j, int i)
+{
+	int result;
+
+	result = (i + j);
+	return (result);
+}
