73432175361b22df2d5d8089bb3d18397d3bc3d0
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-26 11:53:34 -0700

    File 10-print_traingle.c added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle on the screen
+ * @size: parameter
+ *
+ * Return: returns 0
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+
+{
+	_putchar('\n');
+}
+	else
+{
+	int x, y;
+
+	for (x = 1; x <= size; x++)
+{
+	for (y = i; y < size; y++)
+{
+	_putchar(' ');
+}
+	for (y = 1; y <= x; y++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
+}
