da500318c0801a4000024b5bf5c424e5c52544f6
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-26 15:52:42 -0700

    Files added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints if int number is positive or negative
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+	return (0);
+}
