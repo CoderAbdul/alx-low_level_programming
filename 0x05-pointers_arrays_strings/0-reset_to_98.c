f0770088f5f87a72abc782142c88c01dfa216c88
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-27 06:03:31 -0700

    Files added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Take sa pointer to a parameter and changes the value
+ * @n: Input parameter
+ *
+ * Return : No return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
