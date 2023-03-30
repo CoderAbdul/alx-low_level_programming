e4ad8150c5069a78a51cba1b0a80b0f29a0ba035
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-03-30 07:10:14 -0700

    0-strcat.c added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,22 +10,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	unsigned int i;
+	unsigned int j;
 
-	a = 0;
-	b = 0;
+	i = 0;
+	j = 0;
 
-	while (dest[a] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[b] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[a] = src[b];
-		b++;
-		a++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
-	dest[a] = '\0';
+
+	dest[i] = '\0';
 	return (dest);
 }
