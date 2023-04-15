18578a9760b93f5d1b5431111fe960a4bb9c7f53
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-04-15 13:54:56 -0700

    1-args.c fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 0)
 	{
-		argv = argv
+		argv = argv;
 		printf("%d\n", argc - 1);
 	}
 	return (0);
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.b"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argumeng array
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
