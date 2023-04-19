e33f40d8c67817024bcdc5c2b1dfcb8431e9f9d2
CoderAbdul <abdulganiyuajasa@gmail.com>
2023-04-19 00:01:07 -0700

    3-function_like_macro.h added

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,10 @@
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LIKE_MACRO_H_
+/**
+ * file - computes the absolute value of a number
+ * @x: value
+ * Return: an int
+ */
 
 #define ABS(x) ((X) < 0 ? (x) * -1 : (x))
 
